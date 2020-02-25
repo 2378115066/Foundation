a350ebdacf1599b4e1281204ed66d94c450d6afa
daiyuanq <daiyuanq@sina.com>
2020-02-25 23:57:31 +0800

    20200225 commit

---
+++ src/com/daiinfo/accidence/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.daiinfo.accidence;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,the world!");
+		System.out.println("Java,你好！");
+	}
+
+}
---
+++ src/com/daiinfo/grammar/GrammarTest.java
@@ -0,0 +1,11 @@
+package com.daiinfo.grammar;
+
+public class GrammarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println("i=" + i);
+	}
+
+}
