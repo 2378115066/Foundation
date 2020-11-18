b47181f452d30d57aed86da5826f6974df73295d
daiyuanq <41301630+daiyuanq@users.noreply.github.com>
2020-11-18 23:19:28 +0800

    Update GrammarTest.java

--- src/com/daiinfo/grammar/GrammarTest.java
+++ src/com/daiinfo/grammar/GrammarTest.java
@@ -5,7 +5,7 @@ public class GrammarTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 0;
-		System.out.println("i=" + i);
+		System.out.println("i的值是" + i);
 	}
 
 }
